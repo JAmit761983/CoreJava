d5290f71b102cc0b2d8f75d147a5e03739fbe793
Amit <Amit@192.168.0.2>
2018-06-10 12:04:50 +0100

    First Commit

---
+++ J8Test/src/main/java/Java8Test/J8Test/App.java
@@ -0,0 +1,13 @@
+package Java8Test.J8Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
